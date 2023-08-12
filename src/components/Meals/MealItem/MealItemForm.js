d2d137581c95fc6ebf1d7c93943b76f88b5847ff
Devam Doshi <devamdoshi212@gmail.com>
2023-08-12 22:32:43 +0530

    get value in context for cart items

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,27 @@
+import { useRef } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
+  const InputRef = useRef();
+  const formsubmithandler = (event) => {
+    event.preventDefault();
+    const enteredamount = InputRef.current.value;
+    const enteredamountnumber = +enteredamount;
+    if (
+      enteredamount.trim() === 0 ||
+      enteredamountnumber < 1 ||
+      enteredamountnumber > 5
+    ) {
+      return;
+    }
+
+    props.onAddToCart(enteredamountnumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={formsubmithandler}>
       <Input
         label="Amount"
+        ref={InputRef}
         input={{
           type: "number",
           id: "amount",
